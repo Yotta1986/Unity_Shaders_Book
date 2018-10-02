dc7e4d338cff5b9dc762dc2731af54b76a47fd34
Yota <yota@battlecryhq.com>
2018-10-02 15:41:00 +0800

    10.1.1 skybox 10.1.2 cubemao 10.1.3 reflection

---
+++ Assets/_10/RenderCubemapWizard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCubemapWizard: MonoBehaviour {
+
+	public Transform renderFromPosition;
+	public Cubemap cubemap;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onWizardCreate()
+	{
+		GameObject go = new GameObject("CubemapCamera");
+		go.AddComponent<Camera>();
+		go.transform.position = renderFromPosition.position;
+		go.GetComponent<Camera>().RenderToCubemap(cubemap);
+		DestroyImmediate(go);
+	}
+}
